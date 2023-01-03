ecae95eb0ce405ba46f21bbaddcc2619d5bde566
Łukasz Burnos <burnoslukasz@gmail.com>
2023-01-03 17:36:13 +0100

    poprawka, kommitowanie

--- src/main/java/FirstConnectionToGit.java
+++ src/main/java/FirstConnectionToGit.java
@@ -1,6 +1,6 @@
 public class FirstConnectionToGit {
     public static void main(String[] args) {
-        System.out.print("Uczę\nsię\nprogramowania");
+        System.out.print("Uczę\nsię\nprogramowania\ni\nkommitowania");
     }
 }
 
